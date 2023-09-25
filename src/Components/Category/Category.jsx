cdf5b20a54b004d103a7819c0cca030d2367e6b6
TanimShahriar <ssmtsshr@gmail.com>
2023-09-25 14:47:15 +0600

    Color updated for each card and modified something in the UI

--- src/Components/Category/Category.jsx
+++ src/Components/Category/Category.jsx
@@ -1,13 +1,14 @@
 const Category = ({ category }) => {
 
-  const { id, categori, title, image, card_bg, text_color } = category;
+  const { id, categori, title, image, card_bg, text_color, category_bg } = category;
 
   return (
-    <div className={`bg-${card_bg} rounded-lg bg-slate-100 border-purple-600 cursor-pointer`}>
-      <figure><img className="w-full mb-4" src={image} alt="Shoes" /></figure>
+    <div style={{ backgroundColor: card_bg }}
+      className={`rounded-lg border-purple-600 cursor-pointer`}>
+      <figure><img className="w-full mb-5" src={image} alt="Shoes" /></figure>
       <div className="">
-        <h2 className="mx-4 w-1/4 px-3 font-medium py-1 rounded-md bg-red-100">{categori}</h2>
-        <p className={`text-${text_color} mx-4 mb-4 font-medium text-xl`}>{title}</p>
+        <h2 style={{ backgroundColor: category_bg, color: text_color }} className="mx-4 w-1/4 px-3 font-medium py-1 rounded-md ">{categori}</h2>
+        <p className={`text-${text_color} mx-4 mb-4 mt-2 font-medium text-xl`}>{title}</p>
       </div>
     </div>
   );
